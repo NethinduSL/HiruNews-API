826ef86f4f4a5389e4b3f446891fb5ae7982dffa
Jithula Bhasitha <161597041+JithulaB@users.noreply.github.com>
2024-06-27 10:42:47 +0530

    Update express.ts

--- api/express.ts
+++ api/express.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Jithula Bhasitha"));
+app.get("/.index.js", (req, res) => res.send("Jithula Bhasitha"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
