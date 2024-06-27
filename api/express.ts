3accc8d51729d72b2123f5e2dd30d60f934bb23b
Jithula Bhasitha <161597041+JithulaB@users.noreply.github.com>
2024-06-27 10:50:55 +0530

    Delete api directory

--- api/express.ts
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/.index.js", (req, res) => res.send("Jithula Bhasitha"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
